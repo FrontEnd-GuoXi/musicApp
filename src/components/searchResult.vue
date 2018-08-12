06d72b3a8d9e07af10c199581e17d12efcc956a0
GuoXi <1206189299@qq.com>
2018-08-12 14:49:03 +0800

    test

--- src/components/searchResult.vue
+++ src/components/searchResult.vue
@@ -20,6 +20,10 @@ export default {
   props: ['searchInfo'],
   beforeCreate: function () {
     console.log('11')
+    function test () {
+      var z= '一个函数'
+      return z
+    }
   },
   created: function () {
 
